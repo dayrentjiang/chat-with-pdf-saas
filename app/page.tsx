2e3d5dd2363d0c4d4efa595097295317bd5855da
Dayrent Tjiang <dtji0003@student.monash.edu>
2024-12-15 18:48:29 +0800

    finish the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Button } from "@/components/ui/button";
 import {
   BrainCogIcon,
@@ -22,31 +23,31 @@ const features = [
     name: "Store your PDF Documents",
     description:
       "Upload your PDF documents and store them securely in the cloud.",
-    icon: GlobeIcon
+    icon: BrainCogIcon
   },
   {
     name: "Store your PDF Documents",
     description:
       "Upload your PDF documents and store them securely in the cloud.",
-    icon: GlobeIcon
+    icon: EyeIcon
   },
   {
     name: "Store your PDF Documents",
     description:
       "Upload your PDF documents and store them securely in the cloud.",
-    icon: GlobeIcon
+    icon: MonitorSmartphoneIcon
   },
   {
     name: "Store your PDF Documents",
     description:
       "Upload your PDF documents and store them securely in the cloud.",
-    icon: GlobeIcon
+    icon: ServerCogIcon
   },
   {
     name: "Store your PDF Documents",
     description:
       "Upload your PDF documents and store them securely in the cloud.",
-    icon: GlobeIcon
+    icon: ZapIcon
   }
 ];
 
@@ -79,8 +80,9 @@ export default function Home() {
             <Link href="/dashboard">Get Started</Link>
           </Button>
         </div>
-        <div>
-          <div>
+
+        <div className="relative overflow-hidden pt-16">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <Image
               alt="App Screenshot"
               src="https://i.imgur.com/VciRSTI.jpeg"
@@ -88,7 +90,25 @@ export default function Home() {
               height={1442}
               className="mb-[-0%] rounded-xl shadow-2xl ring-1 ring-gray-900/10"
             />
+            <div aria-hidden="true" className="relative">
+              <div className="absolute bottom-0 -inset-x-32 bg-gradient-to-t from-white/95 pt-[5%]" />
+            </div>
           </div>
+        </div>
+        <div className="mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8">
+          <dl className="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
+            {features.map((feature, index) => (
+              <div key={index} className="relative pl-9">
+                <dt className="inline font-semibold text-gray-900">
+                  <feature.icon
+                    aria-hidden="true"
+                    className="absoulte left-1 top-1 h-5 w-5 text-indigo-600"
+                  />
+                </dt>
+                <dd>{feature.name}</dd>
+              </div>
+            ))}
+          </dl>
         </div>
       </div>
     </main>
